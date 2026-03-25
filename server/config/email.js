07b585b698caba3c5494dd760ddc2fa5a5ae764d
sajid <maraballikwt@gmail.com>
2026-03-25 08:43:33 +0530

    fix nodemailer for render

--- server/config/email.js
+++ server/config/email.js
@@ -1,10 +1,13 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,           // ✅ FIXED
+  secure: false,       // ✅ FIXED
+  family: 4,           // ✅ FORCE IPv4 (VERY IMPORTANT)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
